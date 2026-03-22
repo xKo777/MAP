9221cd059462b1e0fbf9f6adff84146ed5996c29
xKo777 <hellomello12853@gmail.com>
2026-03-22 21:26:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // 🔥 Firebase
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+
+<script src="app.js"></script>
+  
 const firebaseConfig = {
   apiKey: "AIzaSyCXJoRlLQloM9ODw23dGYpl5r3GWR5HZgA",
   authDomain: "swat-map-a8286.firebaseapp.com",
